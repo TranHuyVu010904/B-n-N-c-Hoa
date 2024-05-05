d40110ba3456dabccd0340969acda48412ffbcf5
Kien2912 <2224802010835@student.tdmu.edu.vn>
2024-05-05 16:54:59 +0700

    sua

--- phatnguyen/Program.cs
+++ phatnguyen/Program.cs
@@ -16,7 +16,7 @@ namespace Nuochoa
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmProduct());   
+            Application.Run(new frmLogin());   
         }
     }
 }
